1fdd151b09c67fc3a184eb9f035e1126e16e8e16
veroeb <vecheza@gmail.com>
2020-12-07 20:21:10 -0300

    carousel no se ve
    no se ve ni el carousel ni el icono de buscar, ni los iconos de redes sociales en el footer, ni el icono de la pagina

--- Celifood/src/Pages/Index.js
+++ Celifood/src/Pages/Index.js
@@ -5,6 +5,10 @@ import Header from '../Components/Header';
 import Heading from '../Components/Heading';
 import Carousel from '../Components/Carousel';
 
+import '../css/styles.css'
+import '../css/bootstrap.css'
+import '../css/bootstrap.min.css'
+
 export default class Index extends React.Component {
     render(){
         return(
