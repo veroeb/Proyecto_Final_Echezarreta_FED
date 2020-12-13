e7ba704fb3845341640696e713c245fc300745fe
veroeb <vecheza@gmail.com>
2020-12-13 17:54:16 -0300

    arreglado cards, falta mostrar imagenes

--- Celifood/src/Components/Card.js
+++ Celifood/src/Components/Card.js
@@ -11,17 +11,11 @@ import {
 export default class Card extends React.Component {
     render() {
         return (
-            <div>                
-                <div className="container pb-4 ">
-                    <div className="row pt-3 pb-5 text-center">
-                        <div className="col-lg-4 p-4">
-                            <h4 className="my-4">{this.props.item.titulo}</h4>
-                            <img src="{this.props.item.imagen}" alt="" className="w-100 rounded" />
-                            {/* <p class="lead pt-3"></p> */}
-                            <a href="{this.props.item.link}" className="btn btn-success btn-md mt-4 px-5">Ver receta</a>
-                        </div>
-                    </div>
-                </div>
+            <div className="col-lg-4 p-4">
+                <h4 className="my-4">{this.props.item.titulo}</h4>
+                <img src="{this.props.item.imagen}" alt="" className="w-100 rounded" />
+                {/* <p class="lead pt-3"></p> */}
+                <a href="{this.props.item.link}" className="btn btn-success btn-md mt-4 px-5">Ver receta</a>
             </div>
         );
     }
